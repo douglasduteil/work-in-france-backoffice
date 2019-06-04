7210b486027acac97a0a1d1347f30129e8d7af24
Thomas Glatt <tglatt@gmail.com>
2019-06-04 12:26:05 +0200

    fix(monthly-report): fix test to define for more or less 3 months

--- packages/extractor/src/model/dossier-record.model.ts
+++ packages/extractor/src/model/dossier-record.model.ts
@@ -1,4 +1,4 @@
-import { differenceInDays } from "date-fns";
+import { differenceInMonths } from "date-fns";
 import { asDate, IIdentifiable, logger } from "../util";
 
 export interface DSGroup {
@@ -109,7 +109,7 @@ export const isLong = (doc: DossierRecord) => {
     if (!startDate || !endDate) {
         return false;
     }
-    return differenceInDays(startDate, endDate) > 90;
+    return differenceInMonths(endDate, startDate) > 3;
 };
 
 const getState = (dossier: DossierRecord) => dossier.metadata.state;
